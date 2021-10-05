fd7e07eb19d1a5f5b0ff836240d031b168571878
James Lockhart <james.lockhart@sykescottages.co.uk>
2021-10-05 17:13:29 +0100

    Added a local build version for dockerhub images

---
+++ dockerhub.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+VERSION=1.2.1
+
+# Validate the container passes our tests
+docker-compose -f docker-compose.test.yml run --rm sut
+
+# You need to provide your own creds because #security
+docker login
+
+
+docker build --no-cache -t sykescottages/base:$VERSION -t sykescottages/base:latest .
+docker push sykescottages/base:$VERSION
+docker push sykescottages/base:latest
