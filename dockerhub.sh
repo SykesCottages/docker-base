99c5e5d575809535f9842af4c26bbd13856d8b92
James Lockhart <james.lockhart@sykescottages.co.uk>
2022-08-25 09:13:37 +0100

    Cleaned up docker images

--- dockerhub.sh
+++ dockerhub.sh
@@ -8,7 +8,10 @@ docker-compose -f docker-compose.test.yml run --rm sut
 # You need to provide your own creds because #security
 docker login
 
-
 docker build --no-cache -t sykescottages/base:$VERSION -t sykescottages/base:latest .
+
 docker push sykescottages/base:$VERSION
 docker push sykescottages/base:latest
+
+docker rmi sykescottages/base:$VERSION
+docker rmi sykescottages/base:latest
