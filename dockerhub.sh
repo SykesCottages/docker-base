c4b4a07cb90f4609727c3ad6f0700488e64fb8ac
James Lockhart <james.lockhart@sykescottages.co.uk>
2023-07-03 11:54:30 +0100

    Build images in parallel

--- dockerhub.sh
+++ dockerhub.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-
 LATEST_VERSION="ubuntu-22-04"
 
 # You need to provide your own creds because #security
-docker login
+docker login >> /dev/null 2>&1
 
 VERSIONS=( "ubuntu-20-04" "ubuntu-22-04" )
 for VERSION in "${VERSIONS[@]}"
 do
-  docker build --no-cache -t sykescottages/base:${VERSION} $VERSION
-  docker push sykescottages/base:${VERSION}
-
-  if [[ "$LATEST_VERSION" == "$VERSION" ]]; then
-    docker tag sykescottages/base:${VERSION} sykescottages/base:latest
-    docker push sykescottages/base:latest
-    docker rmi sykescottages/base:latest
-  fi
-
-  docker rmi sykescottages/base:${VERSION}
+  ./build.sh $VERSION $LATEST_VERSION >> /dev/null 2>&1 &
 done
+
+wait
