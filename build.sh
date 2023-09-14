e76998db210b2fc4fcca26c6a61d23e61326785e
James Lockhart <james.lockhart@sykescottages.co.uk>
2023-09-14 13:02:44 +0100

    Added the Aptitude Itinerary for the images for historial reference

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ LATEST_VERSION=$2
 
 docker build --quiet --no-cache -t sykescottages/base:${VERSION} $VERSION
 docker push sykescottages/base:${VERSION}
+docker run -it sykescottages/base:${VERSION} bash -c "apt -qq list | tail -n+2" | tail -n+4 > ${VERSION}/aptitude-itinerary
 
 if [[ "$LATEST_VERSION" == "$VERSION" ]]; then
   docker tag sykescottages/base:${LATEST_VERSION} sykescottages/base:latest
