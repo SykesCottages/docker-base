ce96ae6dd798234fbca8a0abb6d68d746603160c
James Lockhart <james.lockhart@sykescottages.co.uk>
2023-09-15 09:39:34 +0100

    Updated versions in README

--- versions.sh
+++ versions.sh
@@ -4,5 +4,6 @@ LATEST_VERSION="ubuntu-22-04"
 VERSIONS=( "ubuntu-20-04" "ubuntu-22-04" )
 for VERSION in "${VERSIONS[@]}"
 do
+  docker run -it sykescottages/base:${VERSION} lsb_release -a
   docker run -it sykescottages/base:${VERSION} bash -c "apt -qq list | tail -n+2" | tail -n+4 > ${VERSION}/aptitude-itinerary
 done
