3e037ac1d913a55f7f15e02cb58a673f8adf8a35
James Lockhart <james.lockhart@sykescottages.co.uk>
2023-11-30 14:44:29 +0000

    Added ARM support

--- dockerhub.sh
+++ dockerhub.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-LATEST_VERSION="ubuntu-22-04"
 
 # You need to provide your own creds because #security
-docker login >> /dev/null 2>&1
+docker login >>/dev/null 2>&1
 
-VERSIONS=( "ubuntu-20-04" "ubuntu-22-04" )
-for VERSION in "${VERSIONS[@]}"
-do
-  ./build.sh $VERSION $LATEST_VERSION >> /dev/null 2>&1 &
+VERSIONS=("ubuntu-20-04" "ubuntu-22-04")
+for VERSION in "${VERSIONS[@]}"; do
+  ./build.sh docker buildx build \
+    --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm64/v8 \
+    --quiet \
+    --no-cache \
+    --push \
+    -t sykescottages/base:${VERSION} \
+    $VERSION >>/dev/null 2>&1 &
 done
 
 wait
