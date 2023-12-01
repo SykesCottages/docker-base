034a1fc8a9aaf9186668f681e047e4424b810b8c
James Lockhart <james.lockhart@sykescottages.co.uk>
2023-12-01 13:27:01 +0000

    Finalised the multi-arch support

--- dockerhub.sh
+++ dockerhub.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+LATEST_VERSION=ubuntu-22-04
+
 # You need to provide your own creds because #security
 docker login >>/dev/null 2>&1
 
 VERSIONS=("ubuntu-20-04" "ubuntu-22-04")
 for VERSION in "${VERSIONS[@]}"; do
-  ./build.sh docker buildx build \
-    --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm64/v8 \
+  docker buildx build \
+    --platform linux/arm/v7,linux/arm64,linux/amd64 \
     --quiet \
     --no-cache \
     --push \
@@ -15,3 +17,11 @@ for VERSION in "${VERSIONS[@]}"; do
 done
 
 wait
+
+docker buildx build \
+  --platform linux/arm/v7,linux/arm64,linux/amd64 \
+  --quiet \
+  --no-cache \
+  --push \
+  -t sykescottages/base:latest \
+  $LATEST_VERSION >>/dev/null 2>&1
