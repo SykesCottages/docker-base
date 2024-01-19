25bb4f67feabee32e21977b060eddadbf49fbaf5
James Lockhart <james.lockhart@sykescottages.co.uk>
2024-01-19 09:50:04 +0000

    Remove manifest before creating new one

--- manifest.sh
+++ manifest.sh
@@ -9,6 +9,7 @@ VERSIONS=("ubuntu-20-04" "ubuntu-22-04")
 
 for VERSION in "${VERSIONS[@]}"; do
   TAG="sykescottages/base:${VERSION}"
+  docker manifest rm $TAG
   docker manifest create $TAG \
       --amend "$TAG-amd64" \
       --amend "$TAG-arm64"
@@ -17,6 +18,7 @@ done
 
 TAG="sykescottages/base:latest"
 LATEST_TAG="sykescottages/base:$LATEST_VERSION"
+docker manifest rm $TAG
 docker manifest create $TAG \
     --amend "$LATEST_TAG-amd64" \
     --amend "$LATEST_TAG-arm64"
