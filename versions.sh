87e604d28bd034c9b00bb27bb66adeb47d08f4a3
James Lockhart <james.lockhart@sykescottages.co.uk>
2024-06-14 14:40:16 +0100

    Package updates

--- versions.sh
+++ versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 LATEST_VERSION="ubuntu-22-04"
 
-VERSIONS=( "ubuntu-20-04" "ubuntu-22-04" )
+VERSIONS=( "ubuntu-20-04" "ubuntu-22-04" "ubuntu-24-04")
 for VERSION in "${VERSIONS[@]}"
 do
   docker run -it sykescottages/base:${VERSION} lsb_release -a
