bab0271027528834829c685e4c2a80ac31b618e7
James Lockhart <james.lockhart@sykescottages.co.uk>
2025-02-28 16:24:37 +0000

    Package updates

--- versions.sh
+++ versions.sh
@@ -4,6 +4,6 @@ LATEST_VERSION="ubuntu-22-04"
 VERSIONS=( "ubuntu-20-04" "ubuntu-22-04" "ubuntu-24-04")
 for VERSION in "${VERSIONS[@]}"
 do
-  docker run -it sykescottages/base:${VERSION} lsb_release -a
-  docker run -it sykescottages/base:${VERSION} bash -c "apt -qq list | tail -n+2" | tail -n+4 > ${VERSION}/aptitude-itinerary
+  docker run --pull always -it sykescottages/base:${VERSION} lsb_release -a
+  docker run --pull always -it sykescottages/base:${VERSION} bash -c "apt -qq list | tail -n+2" | tail -n+4 > ${VERSION}/aptitude-itinerary
 done
